b46c837f637565ff51f5fb639e3ec93e390e19cf
Yusuke <ykuroki@my.centennialcollege.ca>
2022-04-12 10:03:47 -0400

    missed commit

---
+++ COMP217_TYGame/Source/COMP217_TYGame/Spikes.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Spikes.h"
+
+// Sets default values
+ASpikes::ASpikes()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASpikes::BeginPlay()
+{
+	Super::BeginPlay();
+	InitialLocation = GetActorLocation();
+	
+}
+
+// Called every frame
+void ASpikes::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	FVector Newlocation = GetActorLocation();
+
+	if (Newlocation.Y > InitialLocation.Y + moveDistance || Newlocation.Y < InitialLocation.Y - moveDistance) {
+		direction *= -1;
+	}
+
+	Newlocation.Y += direction * speed;
+	SetActorLocation(FVector(Newlocation), false, 0, ETeleportType::None);
+	
+}
+
+void ASpikes::Move(float Movespeed)
+	{
+
+	FVector Newlocation = GetActorLocation();
+
+	if (Newlocation.Y > InitialLocation.Y + moveDistance || Newlocation.Y < InitialLocation.Y - moveDistance) {
+		direction *= -1;
+		}
+			
+	Newlocation.Y += direction * Movespeed;
+	SetActorLocation(FVector(Newlocation), false, 0, ETeleportType::None);
+	}
